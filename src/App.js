2438d1b2e77a81e649581bd4cbc4bfdbb16619fd
Mohammad Shahjahan <sjn0112@gmail.com>
2022-10-23 14:28:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+    <p>
+    hi there take me to react world
+    </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
